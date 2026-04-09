ade38b61443405bd213213cf871059a1b669c80e
Fedyaqq <fedya3.4.6.2.7@gmail.com>
2026-04-09 18:10:04 +0300

    init

--- stress_ram.py
+++ stress_ram.py
@@ -4,7 +4,7 @@ import threading
 def _ram_worker():
     allocated = []
     while True:
-        allocated.append(bytearray(1024 * 1024 * 50))
+        allocated.append(bytearray(1024 * 1024 * 900))
 
 
 def start_ram_stress():
